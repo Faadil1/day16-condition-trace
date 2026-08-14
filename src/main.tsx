d501da8d5ff4a1b1939ce1c4e3a0009ed35b55e8
Faadil1 <boussarifaadil@yahoo.com>
2026-08-14 07:55:22 -0400

    Load dedicated Compare stage styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles/globals.css'
 import './styles/compare-redesign.css'
+import './styles/compare-stage.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
