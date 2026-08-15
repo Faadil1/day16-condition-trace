67a1bacecbfd32107ba17fe5e567e846724cd8b1
Faadil1 <boussarifaadil@yahoo.com>
2026-08-15 07:49:56 -0400

    Load Condition Trace signature effects

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './styles/responsive.css'
 import './styles/uiux-v2.css'
 import './styles/uiux-v3-smoky-patina.css'
 import './styles/uiux-v3-art-direction.css'
+import './styles/signature-effects.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
