91f36b9206835cd49e70d903b7bd1b50aba7aa40
Faadil1 <boussarifaadil@yahoo.com>
2026-08-14 20:42:59 -0400

    Load UIUX V2 conservation desk theme

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/compare-stage.css'
 import './styles/vessel-v2.css'
 import './styles/evidence-lineage.css'
 import './styles/responsive.css'
+import './styles/uiux-v2.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
