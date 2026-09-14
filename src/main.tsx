dc36a537dd08b3e3d835c25fe3cae3b97e6060e4
Faadil1 <boussarifaadil@yahoo.com>
2026-09-14 15:38:18 -0400

    Activate TRACE classical futurist delta v2

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './styles/signature-effects.css'
 import './styles/microinteractions.css'
 import './styles/typography-v3.css'
 import './styles/design-delta-v1.css'
+import './styles/design-delta-v2-classical-futurist.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
